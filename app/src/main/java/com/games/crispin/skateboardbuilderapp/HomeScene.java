5904ef047caf2fd22188a6712ecc31842630b96f
Christian Benner <christianbenner35@gmail.com>
2019-12-10 22:27:54 +0000

    Transition Changes
    Some changes to transitions on the home scene.

--- app/src/main/java/com/games/crispin/skateboardbuilderapp/HomeScene.java
+++ app/src/main/java/com/games/crispin/skateboardbuilderapp/HomeScene.java
@@ -6,11 +6,14 @@ import com.games.crispin.crispinmobile.Crispin;
 import com.games.crispin.crispinmobile.Geometry.Point2D;
 import com.games.crispin.crispinmobile.Geometry.Scale2D;
 import com.games.crispin.crispinmobile.Rendering.Data.Colour;
+import com.games.crispin.crispinmobile.Rendering.Models.Square;
 import com.games.crispin.crispinmobile.Rendering.Utilities.Camera2D;
 import com.games.crispin.crispinmobile.Rendering.Utilities.Font;
+import com.games.crispin.crispinmobile.Rendering.Utilities.Material;
 import com.games.crispin.crispinmobile.Rendering.Utilities.Texture;
 import com.games.crispin.crispinmobile.UserInterface.Button;
 import com.games.crispin.crispinmobile.UserInterface.Image;
+import com.games.crispin.crispinmobile.UserInterface.LinearLayout;
 import com.games.crispin.crispinmobile.UserInterface.Text;
 import com.games.crispin.crispinmobile.Utilities.Scene;
 
@@ -23,17 +26,16 @@ public class HomeScene extends Scene
     private Text newText;
     private Text openText;
     private Text buyText;
+    private Square transitionSquare;
+
+    private LinearLayout buttonLayout;
+    private LinearLayout textLayout;
 
     private Camera2D uiCamera;
 
-    private boolean fadeIn = true;
-    private float uiAlpha = 0.0f;
     private float newUIColour = 1.0f;
     private float openUIColour = 1.0f;
     private float buyUIColour = 1.0f;
-    private boolean holdingNewButton = false;
-    private boolean holdingOpenButton = false;
-    private boolean holdingBuyButton = false;
 
     public HomeScene()
     {
@@ -42,6 +44,10 @@ public class HomeScene extends Scene
 
         // Create a 2D camera for the UI rendering
         uiCamera = new Camera2D();
+
+        transitionSquare = new Square(new Material(new Colour(57, 142, 178)), false);
+        transitionSquare.setPosition(0.0f, 0.0f);
+        transitionSquare.setScale(Crispin.getSurfaceWidth(), Crispin.getSurfaceHeight());
 
         // Load the logo texture
         final Texture logoTexture = new Texture(R.drawable.logo);
@@ -71,55 +77,43 @@ public class HomeScene extends Scene
 
         newText = new Text(buttonFont, "NEW", false, true,
                 buttonSize);
-        newText.setPosition((Crispin.getSurfaceWidth() / 2.0f) - (buttonSize / 2.0f) -
-                buttonPadding - buttonSize, textPosY);
-        newText.setColour(Colour.WHITE);
-
         openText = new Text(buttonFont, "OPEN", false, true,
                 buttonSize);
-        openText.setPosition((Crispin.getSurfaceWidth() / 2.0f) - (buttonSize / 2.0f),
-                textPosY);
-        openText.setColour(Colour.WHITE);
-
         buyText = new Text(buttonFont, "BUY", false, true,
                 buttonSize);
-        buyText.setPosition((Crispin.getSurfaceWidth() / 2.0f) +
-                (buttonSize / 2.0f) + buttonPadding, textPosY);
-        buyText.setColour(Colour.WHITE);
-
         newButton = new Button(new Texture(R.drawable.pencil_icon));
-        newButton.setSize(new Scale2D(buttonSize, buttonSize));
-        newButton.setPosition((Crispin.getSurfaceWidth() / 2.0f) - (buttonSize / 2.0f) -
-                buttonPadding - buttonSize, buttonPosY);
-
         openButton = new Button(new Texture(R.drawable.folder_icon));
-        openButton.setSize(new Scale2D(buttonSize, buttonSize));
-        openButton.setPosition((Crispin.getSurfaceWidth() / 2.0f) - (buttonSize / 2.0f),
-                buttonPosY);
-
         buyButton = new Button(new Texture(R.drawable.bank_card_icon));
-        buyButton.setSize(new Scale2D(buttonSize, buttonSize));
-        buyButton.setPosition((Crispin.getSurfaceWidth() / 2.0f) + (buttonSize / 2.0f) +
-                buttonPadding, buttonPosY);
 
-        logoImage.setOpacity(uiAlpha);
-        newButton.setOpacity(uiAlpha);
-        openButton.setOpacity(uiAlpha);
-        buyButton.setOpacity(uiAlpha);
-        newText.setOpacity(uiAlpha);
-        openText.setOpacity(uiAlpha);
-        buyText.setOpacity(uiAlpha);
+        newText.setColour(Colour.WHITE);
+        openText.setColour(Colour.WHITE);
+        buyText.setColour(Colour.WHITE);
+        newButton.setSize(new Scale2D(buttonSize, buttonSize));
+        openButton.setSize(new Scale2D(buttonSize, buttonSize));
+        buyButton.setSize(new Scale2D(buttonSize, buttonSize));
+
+
+        buttonLayout = new LinearLayout(new Point2D((Crispin.getSurfaceWidth() / 2.0f) - (buttonSize / 2.0f) - buttonPadding - buttonSize, buttonPosY));
+        buttonLayout.setPadding(new Scale2D(buttonPadding, 0.0f));
+        buttonLayout.add(newButton);
+        buttonLayout.add(openButton);
+        buttonLayout.add(buyButton);
+
+        textLayout = new LinearLayout(new Point2D((Crispin.getSurfaceWidth() / 2.0f) - (buttonSize / 2.0f) - buttonPadding - buttonSize, textPosY));
+        textLayout.setPadding(new Scale2D(buttonPadding, 0.0f));
+        textLayout.add(newText);
+        textLayout.add(openText);
+        textLayout.add(buyText);
 
         newButton.addTouchListener(e -> {
             switch (e.getEvent())
             {
                 case CLICK:
                     newText.enableWiggle(40.0f, Text.WiggleSpeed_E.FAST);
-                    holdingNewButton = true;
+                    transitionToScene(SelectDeckWidthScene::new);
                     break;
                 case RELEASE:
                     newText.disableWiggle();
-                    holdingNewButton = false;
                     break;
             }
         });
@@ -129,12 +123,10 @@ public class HomeScene extends Scene
             {
                 case CLICK:
                     openText.enableWiggle(40.0f, Text.WiggleSpeed_E.FAST);
-                    holdingOpenButton = true;
-                    fadeIn = false;
+                    transitionToScene(TestScene::new);
                     break;
                 case RELEASE:
                     openText.disableWiggle();
-                    holdingOpenButton = false;
                     break;
             }
         });
@@ -144,11 +136,9 @@ public class HomeScene extends Scene
             {
                 case CLICK:
                     buyText.enableWiggle(40.0f, Text.WiggleSpeed_E.FAST);
-                    holdingBuyButton = true;
                     break;
                 case RELEASE:
                     buyText.disableWiggle();
-                    holdingBuyButton = false;
                     break;
             }
         });
@@ -156,7 +146,7 @@ public class HomeScene extends Scene
 
     void handleNewButtonColour()
     {
-        if(holdingNewButton)
+        if(newButton.isClicked())
         {
             if(newUIColour <= 0.0f)
             {
@@ -188,7 +178,7 @@ public class HomeScene extends Scene
 
     void handleOpenButtonColour()
     {
-        if(holdingOpenButton)
+        if(openButton.isClicked())
         {
             if(openUIColour <= 0.0f)
             {
@@ -220,7 +210,7 @@ public class HomeScene extends Scene
 
     void handleBuyButtonColour()
     {
-        if(holdingBuyButton)
+        if(buyButton.isClicked())
         {
             if(buyUIColour <= 0.0f)
             {
@@ -250,56 +240,97 @@ public class HomeScene extends Scene
         }
     }
 
-    @Override
-    public void update(float deltaTime) {
-        handleNewButtonColour();
-        handleOpenButtonColour();
-        handleBuyButtonColour();
-
-        if(fadeIn)
+    public void handleTransition()
+    {
+/*        if(fadeIn)
         {
-            if(uiAlpha >= 1.0f)
+            if(transitionSquare.getAlpha() > 0.0f)
             {
-                uiAlpha = 1.0f;
+                transitionSquare.setAlpha(transitionSquare.getAlpha() - 0.05f);
             }
             else
             {
-                uiAlpha += 0.05f;
+                transitionSquare.setAlpha(0.0f);
             }
         }
         else
         {
-            if(uiAlpha <= 0.0f)
+            if(transitionSquare.getAlpha() < 1.0f)
             {
-                uiAlpha = 0.0f;
-
-                Crispin.setScene(TestScene::new);
+                transitionSquare.setAlpha(transitionSquare.getAlpha() + 0.05f);
             }
             else
             {
-                uiAlpha -= 0.05f;
+                transitionSquare.setAlpha(1.0f);
+            }
+        }*/
+
+        if(fadeIn)
+        {
+            if(transitionAlpha <= 0.0f)
+            {
+                transitionAlpha = 0.0f;
+                transitioning = false;
+            }
+            else
+            {
+                transitionAlpha -= 0.05f;
+            }
+        }
+        else
+        {
+            if(transitionAlpha >= 1.0f)
+            {
+                transitionAlpha = 1.0f;
+                transitioning = false;
+                Crispin.setScene(transitionScene);
+            }
+            else
+            {
+                transitionAlpha += 0.05f;
             }
         }
 
+        transitionSquare.setAlpha(transitionAlpha);
+    }
 
-        logoImage.setOpacity(uiAlpha);
-        newButton.setOpacity(uiAlpha);
-        openButton.setOpacity(uiAlpha);
-        buyButton.setOpacity(uiAlpha);
-        newText.setOpacity(uiAlpha);
-        openText.setOpacity(uiAlpha);
-        buyText.setOpacity(uiAlpha);
+    boolean fadeIn = true;
+    boolean transitioning = true;
+    float transitionAlpha = 1.0f;
+    Scene.Constructor transitionScene;
+    private void transitionToScene(Scene.Constructor scene)
+    {
+        transitionScene = scene;
+        transitioning = true;
+        transitionAlpha = 0.0f;
+        transitionSquare.setAlpha(0.0f);
+        fadeIn = false;
+    }
+
+    @Override
+    public void update(float deltaTime)
+    {
+        handleNewButtonColour();
+        handleOpenButtonColour();
+        handleBuyButtonColour();
+
+        if(transitioning)
+        {
+            handleTransition();
+        }
+
     }
 
     @Override
     public void render() {
         logoImage.draw(uiCamera);
-        newButton.draw(uiCamera);
-        openButton.draw(uiCamera);
-        buyButton.draw(uiCamera);
-        newText.draw(uiCamera);
-        openText.draw(uiCamera);
-        buyText.draw(uiCamera);
+        buttonLayout.draw(uiCamera);
+        textLayout.draw(uiCamera);
+
+        if(transitioning)
+        {
+            transitionSquare.render(uiCamera);
+        }
     }
 
     @Override
--- app/src/main/java/com/games/crispin/skateboardbuilderapp/SelectDeckWidthScene.java
+++ app/src/main/java/com/games/crispin/skateboardbuilderapp/SelectDeckWidthScene.java
@@ -1,10 +1,18 @@
 package com.games.crispin.skateboardbuilderapp;
 
+import com.games.crispin.crispinmobile.Crispin;
 import com.games.crispin.crispinmobile.Geometry.Point2D;
+import com.games.crispin.crispinmobile.Rendering.Data.Colour;
 import com.games.crispin.crispinmobile.Utilities.Scene;
 
 public class SelectDeckWidthScene extends Scene
 {
+    public SelectDeckWidthScene()
+    {
+        // Set the background to a blue colour
+        Crispin.setBackgroundColour(new Colour(0, 0, 0));
+    }
+
     @Override
     public void update(float deltaTime)
     {
