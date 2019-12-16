51ed7e0ff78de4fde78495fc5102a1a0848e2867
Christian Benner <christianbenner35@gmail.com>
2019-12-16 15:48:07 +0000

    Dropdown Test
    Testing dropdown UI by allowing the user to select different models to load.

--- app/src/main/java/com/games/crispin/skateboardbuilderapp/SelectDeckWidthScene.java
+++ app/src/main/java/com/games/crispin/skateboardbuilderapp/SelectDeckWidthScene.java
@@ -1,27 +1,35 @@
 package com.games.crispin.skateboardbuilderapp;
 
-import android.transition.Fade;
-
 import com.games.crispin.crispinmobile.Crispin;
 import com.games.crispin.crispinmobile.Geometry.Point2D;
+import com.games.crispin.crispinmobile.Geometry.Point3D;
+import com.games.crispin.crispinmobile.Geometry.RotationMatrix;
+import com.games.crispin.crispinmobile.Geometry.Vector3D;
 import com.games.crispin.crispinmobile.Rendering.Data.Colour;
 import com.games.crispin.crispinmobile.Rendering.Utilities.Camera2D;
-import com.games.crispin.crispinmobile.UserInterface.Button;
+import com.games.crispin.crispinmobile.Rendering.Utilities.Camera3D;
+import com.games.crispin.crispinmobile.Rendering.Utilities.Material;
+import com.games.crispin.crispinmobile.Rendering.Utilities.RenderObject;
+import com.games.crispin.crispinmobile.Rendering.Utilities.Texture;
 import com.games.crispin.crispinmobile.UserInterface.Dropdown;
-import com.games.crispin.crispinmobile.UserInterface.Plane;
 import com.games.crispin.crispinmobile.UserInterface.TouchEvent;
 import com.games.crispin.crispinmobile.UserInterface.TouchListener;
+import com.games.crispin.crispinmobile.Utilities.OBJModelLoader;
 import com.games.crispin.crispinmobile.Utilities.Scene;
 
 public class SelectDeckWidthScene extends Scene
 {
     // Camera for 2D/user interface rendering
     private Camera2D uiCamera;
+    private Camera3D camera3D;
+
     private Dropdown widthSelectDropdown;
 
     private FadeTransition fadeTransition;
 
     private CustomButton backButton;
+
+    private RenderObject model;
 
     public SelectDeckWidthScene()
     {
@@ -34,12 +42,40 @@ public class SelectDeckWidthScene extends Scene
 
         uiCamera = new Camera2D(0, 0, Crispin.getSurfaceWidth(), Crispin.getSurfaceHeight());
 
-        widthSelectDropdown = new Dropdown();
-        widthSelectDropdown.addItem("8.125");
-        widthSelectDropdown.addItem("8.25");
-        widthSelectDropdown.addItem("8.375");
-        widthSelectDropdown.addItem("8.5");
-        widthSelectDropdown.addItem("8.6");
+/*        widthSelectDropdown = new Dropdown("Select a deck width");
+        widthSelectDropdown.setPosition(Crispin.getSurfaceWidth() * 0.05f, Crispin.getSurfaceHeight() * 0.7f);
+        widthSelectDropdown.setSize(Crispin.getSurfaceWidth() * 0.9f, 100.0f);
+        widthSelectDropdown.setDisabledBorders(Dropdown.INNER_BORDERS);
+        widthSelectDropdown.setColour(HomeScene.BACKGROUND_COLOR);
+        widthSelectDropdown.setTextColour(Colour.WHITE);
+        widthSelectDropdown.setBorderColour(Colour.WHITE);
+        widthSelectDropdown.setStateIcons(R.drawable.expand_icon, R.drawable.collapse_icon);
+
+        final int id8125 = widthSelectDropdown.addItem("8.125");
+        final int id825 = widthSelectDropdown.addItem("8.25");
+        final int id8375 = widthSelectDropdown.addItem("8.375");
+        final int id85 = widthSelectDropdown.addItem("8.5");
+        final int id86 = widthSelectDropdown.addItem("8.6");*/
+
+        camera3D = new Camera3D();
+        camera3D.setPosition(new Point3D(0.0f, 0.0f, 7.0f));
+
+        widthSelectDropdown = new Dropdown("Select an object to view");
+        widthSelectDropdown.setPosition(Crispin.getSurfaceWidth() * 0.05f, Crispin.getSurfaceHeight() * 0.7f);
+        widthSelectDropdown.setSize(Crispin.getSurfaceWidth() * 0.9f, 100.0f);
+        widthSelectDropdown.setDisabledBorders(Dropdown.INNER_BORDERS);
+        widthSelectDropdown.setColour(HomeScene.BACKGROUND_COLOR);
+        widthSelectDropdown.setTextColour(Colour.WHITE);
+        widthSelectDropdown.setBorderColour(Colour.WHITE);
+        widthSelectDropdown.setStateIcons(R.drawable.expand_icon, R.drawable.collapse_icon);
+
+        final int trucks = widthSelectDropdown.addItem("Trucks");
+        final int deckOne = widthSelectDropdown.addItem("Deck One");
+        final int deckTwo = widthSelectDropdown.addItem("Deck Two");
+
+        Material grey = new Material(new Texture(R.drawable.grey));
+        Material palace = new Material(new Texture(R.drawable.palacedeck));
+        Material jart = new Material(new Texture(R.drawable.jart_new_wave));
 
         widthSelectDropdown.addTouchListener(new TouchListener() {
             @Override
@@ -47,18 +83,43 @@ public class SelectDeckWidthScene extends Scene
                 switch (e.getEvent())
                 {
                     case CLICK:
-                        if(widthSelectDropdown.isExpanded())
+
+                        break;
+                    case RELEASE:
+                        int selectedId = widthSelectDropdown.getSelectedId();
+
+                        if(selectedId == trucks)
                         {
-                            widthSelectDropdown.collapse();
+                            model = OBJModelLoader.readObjFile(R.raw.trucktest);
+                            model.setScale(0.6f, 0.6f, 0.6f);
+                            model.setMaterial(grey);
+                        }
+                        else if(selectedId == deckOne)
+                        {
+                            model = OBJModelLoader.readObjFile(R.raw.deck8_125_uv_test);
+                            model.setMaterial(palace);
+                            model.setScale(0.2f, 0.2f, 0.2f);
+                        }
+                        else if(selectedId == deckTwo)
+                        {
+                            model = OBJModelLoader.readObjFile(R.raw.deck8_125_uv_test);
+                            model.setMaterial(jart);
+                            model.setScale(0.2f, 0.2f, 0.2f);
                         }
                         else
                         {
-                            widthSelectDropdown.expand();
+                            System.out.println("ooo No selection");
                         }
                         break;
-                    case RELEASE:
+                }
 
-                        break;
+                if(model != null)
+                {
+                    RotationMatrix rm = new RotationMatrix();
+                    rm.reset();
+                    rm.applyRotation(new Vector3D(0.0f, 1.0f, 0.0f), 45.0f + 45.0f);
+                    rm.applyRotation(new Vector3D(1.0f, 0.0f, 0.0f), 45.0f + 270.0f);
+                    model.setRotation(rm);
                 }
             }
         });
@@ -94,6 +155,11 @@ public class SelectDeckWidthScene extends Scene
         backButton.draw(uiCamera);
         widthSelectDropdown.draw(uiCamera);
         fadeTransition.draw(uiCamera);
+
+        if(model != null)
+        {
+            model.newRender(camera3D);
+        }
     }
 
     @Override
